2ae4e3f0bc79ce82f5a5a6efa2b5f330810c8c6e
Aleksei Koziatinskii <alekseik@netflix.com>
2021-10-15 22:43:45 -0700

    implemented select
    - how to do icon without maring / padding tricks?
    - how to avoid text measurement for auto resizing?

--- src/components/Select/Select.helpers.js
+++ src/components/Select/Select.helpers.js
@@ -8,3 +8,11 @@ export function getDisplayedValue(value, children) {
 
   return selectedChild.props.children;
 }
+
+export function getTextWidth(text, font) {
+  const canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+  const context = canvas.getContext("2d");
+  context.font = font;
+  const metrics = context.measureText(text);
+  return metrics.width;
+}
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,15 +3,57 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { getDisplayedValue, getTextWidth } from "./Select.helpers";
+
+const Wrapper = styled.div`
+  color: ${COLORS.gray700};
+  &:hover {
+    color: black;
+  }
+`;
+
+const SelectWrapper = styled.select`
+  border-radius: 8px;
+  border: none;
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+
+  font-family: Roboto,sans-serif;
+  font-weight: 400;
+
+  padding: 12px var(--horizontalPadding);
+  padding-right: calc(var(--horizontalPadding) + var(--arrowWidth) + var(--horizontalPadding));
+  color: currentColor;
+  width: var(--selectWidth);
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  right: calc(var(--horizontalPadding) + var(--arrowWidth));
+  top: 8px;
+  margin-right: calc(-(var(--horizontalPadding) + var(--arrowWidth)));
+  pointer-events: none;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
+  const text = getDisplayedValue(value, children);
+  const arrowWidth = 24;
+  const horizontalPadding = 16;
+  const width = getTextWidth(text, '400 1rem Roboto') + horizontalPadding * 3 + arrowWidth;
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper style={{
+      '--arrowWidth': arrowWidth + 'px',
+      '--horizontalPadding': horizontalPadding + 'px',
+      '--selectWidth': width + 'px',
+    }}>
+      <SelectWrapper value={value} onChange={onChange}>
+        {children}
+      </SelectWrapper>
+      <IconWrapper>
+        <Icon id='chevron-down' size={arrowWidth} strokeWidth={2}></Icon>
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
